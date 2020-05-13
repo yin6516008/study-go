e96e60fb416aba2e4ec101619ed152b2ada500a0
yinming <492960429@qq.com>
2020-05-14 00:29:59 +0800

    day01

---
+++ src/git.jc-ai.cn/yinming/day01/helloworld/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// 声明全局变量
+var (
+	name string
+	age  int
+	isOk bool
+)
+
+func main() {
+	name = "yin"
+	age = 18
+	isOk = true
+
+	// 类型推导
+	gener := "man"
+
+	// 匿名变量
+	_ = name
+
+	fmt.Printf("name: %s\n", name)
+	fmt.Println(age)
+	fmt.Println(isOk)
+	fmt.Println(gener)
+}
