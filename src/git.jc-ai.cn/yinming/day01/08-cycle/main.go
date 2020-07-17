c71e1a39b0a6086d75cbdf601c7540e54b71189a
Administrator <admin@example.com>
2020-07-17 18:35:19 +0800

    aa

--- src/git.jc-ai.cn/yinming/day01/08-cycle/main.go
+++ src/git.jc-ai.cn/yinming/day01/08-cycle/main.go
@@ -4,22 +4,39 @@ import (
 	"fmt"
 )
 
+// func main() {
+// 	for i := 0; i <= 10; i++ {
+// 		switch i {
+// 		case 5:
+// 			goto five
+// 		case 6:
+// 			goto six
+// 		}
+// 		fmt.Println(i)
+// 		if i == 5 {
+// 			break
+// 		}
+// 	}
+
+// five:
+// 	fmt.Println("five")
+
+// 	fmt.Println("seven")
+
+// six:
+// 	fmt.Println("six")
+
+// }
+
 func main() {
-	for i := 0; i <= 10; i++ {
-		switch i {
-		case 5:
-			goto five
-		case 6:
-			goto six
-		}
-		fmt.Println(i)
-		if i == 5 {
-			break
+BREAKDEMO1:
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			if j == 2 {
+				break BREAKDEMO1
+			}
+			fmt.Printf("%v-%v\n", i, j)
 		}
 	}
-five:
-	fmt.Println("five")
-six:
-	fmt.Println("six")
-
+	fmt.Println("...")
 }
