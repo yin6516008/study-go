921d2705108c121e6c0a18f2cb29498ee8a8ec43
Administrator <admin@example.com>
2020-08-11 18:47:27 +0800

    ss

--- src/yin.github.com/yinming/pkgmanage/main.go
+++ src/yin.github.com/yinming/pkgmanage/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "pkgmanage/api"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
 
 func main() {
-	api.BaiDu()
+	s := "hello_world_ff"
+	s = strings.Replace(s, "_", " ", -1)
+	s = strings.Title(s)
+	s = strings.Replace(s, " ", "", -1)
+	fmt.Printf("%T\n", s[0])
+	fmt.Println(string(unicode.ToLower(rune(s[0]))) + s[1:])
+	// s = strings.ToLower() + s[1:]
+	fmt.Println(s)
 }
